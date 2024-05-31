76f1027948e65f6979e877d35b643e893693bdd0
Joshua Morris <joshm@SeattleAudubon.local>
2024-05-31 09:14:42 -0700

    Clean up code, added comments, added new diversity indices, added code for visualizations to explore species trends over time at each park

--- Code/species_diversity.R
+++ Code/species_diversity.R
@@ -8,20 +8,20 @@ library(ggthemes)
 dat <- read_csv("Data/NBP_2023-10-02.csv")
 
 # Inspect data
-str(dat)
+# str(dat)
 
 # Tidy data
-yr <- c(2005:2019, 2021, 2022) # Exclude years prior to 2005, 2020, and 2023
+yr <- c(2005:2019, 2021, 2022) # Vector with years to exclude from data
 
-df <- dat %>%
-  select(-notes) %>%
+df <- dat %>%  # filter undesired years, remove observations not ID'ed to species level, remove hybirds, create unique ID for individual surveys
+ select(-notes) %>%
   filter(Year %in% yr,
          !grepl("sp.", Species), 
          !grepl(" x ", Species)) %>%
   mutate(SurveyID = paste(Year, Month, UniqueStationID, sep = ""))
 
-sort(unique(df$Year))  ## Check filtered out undesired years
-unique(df$sp) ## Should return just "N"
+# sort(unique(df$Year))  ## Check filtered out undesired years
+# unique(df$sp) ## Should return just "N"
 
 
 #### Update taxonomy / nomenclature
@@ -31,7 +31,7 @@ df[df$Species == "Pacific-slope Flycatcher", ]$Species <- "Western Flycatcher"
 df[df$Species == "Mew Gull", ]$Species <- "Short-billed Gull"
 df[df$Species == "Thayer's Gull", ]$Species <- "Iceland Gull"
 
-sort(unique(df$Species))  ## Verify current names in species list
+# sort(unique(df$Species))  ## Verify current names in species list
 
 ## Species response matrix for each individual survey station by year
 srm1 <- df %>%
@@ -60,19 +60,25 @@ srm3 <- df %>%
 
 srm3[is.na(srm3)] <- 0
 
-## Species richness dataframes
+## Diversity data frames
 
 S1 <- data.frame(Year = srm1$Year, 
                 Park = srm1$Park, 
                 UniqueStationID = srm1$UniqueStationID, 
-                S = specnumber(srm1[, 4:204]))
+                S = specnumber(srm1[, 4:204]),
+                H = diversity(srm1[, 4:204]), 
+                Simp = diversity(srm1[, 4:204], index = "simpson"))
 
 S2 <- data.frame(Year = srm2$Year, 
                  Park = srm2$Park, 
-                 S = specnumber(srm2[, 3:203]))
+                 S = specnumber(srm2[, 3:203]),
+                 H = diversity(srm2[, 3:203]), 
+                 Simp = diversity(srm2[, 3:203], index = "simpson"))
 
 S3 <- data.frame(Year = srm3$Year, 
-                 S = specnumber(srm3[, 2:202]))
+                 S = specnumber(srm3[, 2:202]),
+                 H = diversity(srm3[, 2:202]), 
+                 Simp = diversity(srm3[, 2:202], index = "simpson"))
 
 
 ## Plot Species Richness
@@ -90,37 +96,81 @@ ggplot(S2, aes(Year, S, colour = Park)) +
   theme_clean()
 
 ## All survey stations all years
-ggplot(S, aes(x = Year, y = S, colour = Park)) +
+ggplot(S1, aes(x = Year, y = S, colour = Park)) +
   geom_point() +
   geom_jitter() +
   geom_smooth(method = "lm", se = FALSE) +
   theme_clean()
 
-### Plot is difficult to read.
+### Last plot is difficult to read.
 
-parks <- sort(unique(S$Park))
+## Other species richness visualizations 
+
+# Plot species richness for individual count circles over time
+
+parks <- sort(unique(S2$Park))
 nparks <- length(parks)
 
-par(mfrow = c(3, 3))
+par(mfrow = c(3, 4))
 
 for(i in 1:nparks){
-  x <- S[S$Park == parks[i], ]$Year
-  y <- S[S$Park == parks[i], ]$S
+  x <- S1[S1$Park == parks[i], ]$Year
+  y <- S1[S1$Park == parks[i], ]$S
   m <- lm(y ~ x)
-  plot(x, y, main = parks[i], ylab = "S", xlab = "Year")
+  plot(x, y, main = parks[i], ylab = "S", xlab = "Year", xlim = c(2005, 2022), ylim = c(0, 50))
   abline(m, col = "red")
 }
 
+# Plot richness for parks over time
+par(mfrow = c(3, 4))
+
+for(i in 1:nparks){
+  x <- S2[S2$Park == parks[i], ]$Year
+  y <- S2[S2$Park == parks[i], ]$S
+  m <- lm(y ~ x)
+  plot(x, y, main = parks[i], ylab = "S", xlab = "Year", xlim = c(2005, 2022), ylim = c(0, 130))
+  abline(m, col = "red")
+}
+
+# Plot total combined richness over time
+
 par(mfrow = c(1, 1))
-plot(S[S$Park == parks[1], ]$Year, S[S$Park == parks[1], ]$S, main = parks[1], ylab = "S", xlab = "Year")
-lines(predict(lm(S[S$Park == parks[1], ]$S ~ S[S$Park == parks[1], ]$Year), col = "green"))
-y <- S[S$Park == parks[1], ]$S
-x <- S[S$Park == parks[1], ]$Year
+plot(S3$Year, S3$S, ylab = "S", xlab = "Year", xlim = c(2005, 2022), ylim = c(0, 200),
+     main = "Combined Species Richness")
+abline(lm(S3$S ~ S3$Year), col = "red")
 
-plot(x, y, ylim = c(0, 50))
-m <- lm(y ~ x)
-abline(m, col = "red")
+#### RARE SPECIES ####
 
+### Are there any species that only appear once in the data? 
+
+## Create a species response matrix for each individual survey
+ones <- df %>%
+  group_by(SurveyID, Species) %>%
+  summarise(Count = sum(Observed)) %>%
+  pivot_wider(names_from = Species, values_from = Count) %>%
+  ungroup()
+
+ones[is.na(ones)] <- 0  ## replace NA values with zero
+
+ones <- ones[, 2:202] %>%  ## convert to detected / not detected, then calculate column sums
+  mutate_if(is.numeric, ~1 * (. != 0)) %>%
+  colSums()
+
+ones[ones == 1] ## 22 species appear only once in data set,
+ones[ones == 2] ## 17 species appear only twice
+ones[ones == 3] ## 6 species appear only three times
+
+
+## Visualizing diversity indices
+## Shannon-Weaver and Simpson Indices evaluate both species richness, abundance, and evennes.
+
+plot(S3$Year, S3$H)
+abline(lm(S3$H ~ S3$Year), col = 'red')
+
+plot(S3$Year, S3$Simp)
+abline(lm(S3$Simp ~ S3$Year), col = 'red')
+
+## Both diversity indices appear to decreasing over time. 
 
 ## Now create a detection / mean abundance per survey dataframe.  
 det <- df %>%
@@ -139,7 +189,48 @@ det <- det %>%
   mutate(Det.Prop = Det.Count / Surveys, MAPS = Abundance / Surveys)  # MAPS = mean abundance per survey
 
 
-## Inspect dataframe
+## Inspect data frame
 str(det)
 summary(det)
 view(det)
+
+
+## Explore proportion of surveys reporting
+
+## Plot every combination of species and park (NOTE: 201 x 10 charts)
+## Break it down into two batches
+
+species <- sort(unique(det$Species))
+sp1 <- species[1:100]
+sp2 <- species[101:201]
+parks <- sort(unique(det$Park))
+nparks <- length(parks)
+
+# BATCH 1-100
+par(mfrow = c(4, 4))
+
+for(i in 1:length(sp1)) {
+  d <- det[det$Species == sp1[i], ]
+  for(j in 1:nparks) {
+    x <- d[d$Park == parks[j], ]$Year
+    y <- d[d$Park == parks[j], ]$Det.Prop
+    ymax <- case_when(max(y) > 0 ~ max(y), TRUE ~ 0.1)
+    plot(x, y, xlab = "Year", ylab = "Percent reporting", main = paste(species[i], parks[j]),
+         xlim = c(2005, 2022), ylim = c(0, ymax))
+  }
+}
+
+# BATCH 101-201
+
+par(mfrow = c(4, 4))
+
+for(i in 1:length(sp2)) {
+  d <- det[det$Species == sp2[i], ]
+  for(j in 1:nparks) {
+    x <- d[d$Park == parks[j], ]$Year
+    y <- d[d$Park == parks[j], ]$Det.Prop
+    ymax <- case_when(max(y) > 0 ~ max(y), TRUE ~ 0.1)
+    plot(x, y, xlab = "Year", ylab = "Percent reporting", main = paste(species[i], parks[j]),
+         xlim = c(2005, 2022), ylim = c(0, ymax))
+  }
+}
